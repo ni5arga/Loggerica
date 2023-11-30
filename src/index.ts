114319ba487fe5f52001f5144b10c1bb35ce3889
Nisarga Adhikary <git@nisarga.me>
2023-11-30 15:57:00 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,13 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildWebhooks,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildMessageReactions, 
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildModeration,
     ],
 });
 
@@ -200,6 +204,334 @@ client.on('messageReactionRemove', (reaction, user) => {
         messageLogChannel.send({ embeds: [embed] });
     }
 });
+
+client.on('roleCreate', (role) => {
+    const embed = createLogEmbed('Role Created', `Role created: ${role.name}`);
+    embed.setColor('Purple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('roleDelete', (role) => {
+    const embed = createLogEmbed('Role Deleted', `Role deleted: ${role.name}`);
+    embed.setColor('Purple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('channelCreate', (channel) => {
+    const embed = createLogEmbed('Channel Created', `Channel created: ${channel.name}`);
+    embed.setColor('Green');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+
+client.on('channelDelete', (channel) => {
+    if ('name' in channel) {
+        const embed = createLogEmbed('Channel Deleted', `Channel deleted: ${channel.name}`);
+        embed.setColor('Red');
+
+        const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+        if (modLogChannel) { 
+            modLogChannel.send({ embeds: [embed] });
+        }
+    }
+});
+
+client.on('channelUpdate', (oldChannel, newChannel) => {
+    if ('name' in newChannel) {
+        const embed = createLogEmbed('Channel Permissions Updated', `Channel permissions updated: ${newChannel.name}`);
+        embed.setColor('Yellow');
+
+        const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+        if (modLogChannel) {
+            modLogChannel.send({ embeds: [embed] });
+        }
+    }
+});
+
+client.on('guildUpdate', (oldGuild, newGuild) => {
+    if (oldGuild.name !== newGuild.name) {
+        const embed = createLogEmbed('Server Name Change', `Server name changed: ${oldGuild.name} -> ${newGuild.name}`);
+        embed.setColor('Gold');
+
+        const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+        if (modLogChannel) {
+            modLogChannel.send({ embeds: [embed] });
+        }
+    }
+
+    if (oldGuild.icon !== newGuild.icon) {
+        const embed = createLogEmbed('Server Logo Change', 'Server logo changed');
+        embed.setColor('Gold');
+
+        const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+        if (modLogChannel) {
+            modLogChannel.send({ embeds: [embed] });
+        }
+    }
+});
+
+client.on('channelPinsUpdate', (channel, time) => {
+    const embed = createLogEmbed('Channel Pins Update', `Pins updated in ${channel.toString()}`);
+    embed.setColor('Purple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('guildBanAdd', (ban) => {
+    const embed = createLogEmbed('Guild Ban Added', `User banned: ${ban.user.tag}`);
+    embed.setColor('DarkRed');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('guildBanRemove', (ban) => {
+    const embed = createLogEmbed('Guild Ban Removed', `User unbanned: ${ban.user.tag}`);
+    embed.setColor('Green');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('guildIntegrationsUpdate', () => {
+    const embed = createLogEmbed('Guild Integrations Update', 'Guild integrations updated');
+    embed.setColor('LightGrey');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stickerCreate', (sticker) => {
+    const embed = createLogEmbed('Sticker Created', `Sticker created: ${sticker.name}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stickerDelete', (sticker) => {
+    const embed = createLogEmbed('Sticker Deleted', `Sticker deleted: ${sticker.name}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stickerUpdate', (oldSticker, newSticker) => {
+    const embed = createLogEmbed('Sticker Updated', `Sticker updated: ${oldSticker.name} -> ${newSticker.name}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('emojiCreate', (emoji) => {
+    const embed = createLogEmbed('Emoji Created', `Emoji created: ${emoji}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('emojiDelete', (emoji) => {
+    const embed = createLogEmbed('Emoji Deleted', `Emoji deleted: ${emoji}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('emojiUpdate', (oldEmoji, newEmoji) => {
+    const embed = createLogEmbed('Emoji Updated', `Emoji updated: ${oldEmoji} -> ${newEmoji}`);
+    embed.setColor('DarkPurple');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('inviteCreate', (invite) => {
+    const embed = createLogEmbed('Invite Created', `Invite created: ${invite}`);
+    embed.setColor('Gold');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('inviteDelete', (invite) => {
+    const embed = createLogEmbed('Invite Deleted', `Invite deleted: ${invite}`);
+    embed.setColor('Gold');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('threadCreate', (thread) => {
+    const embed = createLogEmbed('Thread Created', `Thread created: ${thread.name}`);
+    embed.setColor('Blue');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('threadDelete', (thread) => {
+    const embed = createLogEmbed('Thread Deleted', `Thread deleted: ${thread.name}`);
+    embed.setColor('Blue');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('threadUpdate', (oldThread, newThread) => {
+    const embed = createLogEmbed('Thread Updated', `Thread updated: ${oldThread.name} -> ${newThread.name}`);
+    embed.setColor('Blue');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stageInstanceCreate', (stageInstance) => {
+    const embed = createLogEmbed(
+        'Stage Instance Created',
+        `Stage instance created: ${stageInstance.topic}`,
+        undefined,
+        undefined
+    );
+    embed.setColor('Blue');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stageInstanceUpdate', (oldStageInstance, newStageInstance) => {
+    const embed = createLogEmbed(
+        'Stage Instance Updated',
+        `Stage instance updated: ${oldStageInstance?.topic ?? 'Unknown'} -> ${newStageInstance.topic}`,
+        undefined,
+        undefined
+    );
+    embed.setColor('Yellow');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('stageInstanceDelete', (stageInstance) => {
+    const embed = createLogEmbed(
+        'Stage Instance Deleted',
+        `Stage instance deleted: ${stageInstance.topic}`,
+        undefined,
+        undefined
+    );
+    embed.setColor('Red');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('messageDeleteBulk', (messages) => {
+    const channel = messages.first()?.guild?.channels.cache.get(messages.first()!.channel.id);
+    const channelName = channel ? `#${channel.name}` : 'Direct Message';
+
+    const author = messages.first()?.author || undefined;
+
+    const embed = createLogEmbed(
+        'Bulk Message Delete',
+        `Bulk deleted messages in ${channelName}:\n${messages.size} messages deleted`,
+        author
+    );
+    embed.setColor('Red');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('messageReactionRemoveAll', (message) => {
+    const channel = message.guild?.channels.cache.get(message.channel.id);
+    const channelName = channel ? `#${channel.name}` : 'Direct Message';
+
+    const embed = createLogEmbed(
+        'All Reactions Removed',
+        `All reactions removed from a message in ${channelName}:\n[Jump to Message](${message.url})`
+    );
+    embed.setColor('Orange');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('webhookCreate', (webhook) => {
+    const embed = createLogEmbed('Webhook Created', `Webhook created: ${webhook.name}`);
+    embed.setColor('DarkOrange');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
+client.on('webhookDelete', (webhook) => {
+    const embed = createLogEmbed('Webhook Deleted', `Webhook deleted: ${webhook.name}`);
+    embed.setColor('DarkOrange');
+
+    const modLogChannel = client.channels.cache.get(modLogChannelID) as TextChannel | undefined;
+    if (modLogChannel) {
+        modLogChannel.send({ embeds: [embed] });
+    }
+});
+
 
 
 client.login(token);
